a529a63c6cb7bfbdbc0ac343ba09d5704ef8caf6
p22yxi <p22yxi@fairfieldprep.org>
2021-10-26 10:14:06 -0400

    lab4-1(need fix)

---
+++ lab_4-1.py
@@ -0,0 +1,12 @@
+# Author Yongdong Xi
+
+print("Fairfield" + " Prep")
+
+print('wei shen mei wo de yan li chang han lei shui, yin wei wo dui zhe pian tu di ai de shen chen ')
+
+print("I 'm loving this short story I 'm reading, \"The Fall of the House of Usher\"")
+sentence = "I 'm loving this short story I 'm reading, \"The Fall of the House of Usher\""
+print(len(sentence))
+
+str("apple")
+print(str("apple"(3)))
