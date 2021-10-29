0b752ec0ef8a075a647ff1da12a51ea6ccc8ab81
p22yxi <p22yxi@fairfieldprep.org>
2021-10-29 10:48:54 -0400

    lab6

---
+++ lab_6-1.py
@@ -0,0 +1,8 @@
+# Author Yongdong Xi (Oct29)
+
+string = "flibbertigibbet"
+string.find("t")
+print(string.find("t"))
+
+sentence = "I wish, I wish, I was a fish."
+print(sentence.split(','))
---
+++ lab_6-2.py
@@ -0,0 +1,6 @@
+# Author Yongdong Xi (Oct29)
+
+name1 = "Alice"
+name2 = "yinzi"
+fs = "Hi, {0}, My name is {1} ".format(name1, name2)
+print(fs)
